9b9b274f705f48114199315ba19f158614708cf4
Felipe <yfsegura04@soy.sena.edu.co>
2023-04-04 16:21:41 -0500

    v.1

---
+++ app/src/main/java/com/felipe/miapp/Colores.java
@@ -0,0 +1,14 @@
+package com.felipe.miapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Colores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colores);
+    }
+}
